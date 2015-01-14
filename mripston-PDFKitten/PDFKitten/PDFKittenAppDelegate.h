a556254732970088cb71eee6cfafb79b45575b6b
CharlyZhang <chicboi@hotmail.com>
2015-01-14 18:59:07 +0800

    删除了PDFKitten项目

--- mripston-PDFKitten/PDFKitten/PDFKittenAppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "RootViewController.h"
-
-@interface PDFKittenAppDelegate : NSObject <UIApplicationDelegate> {
-	IBOutlet RootViewController *rootViewController;
-}
-
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-@end
